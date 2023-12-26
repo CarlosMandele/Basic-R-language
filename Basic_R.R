6b65ea5e649b91cf7ab52701929e5b3c6974088f
CarlosMandele <carlos.mandelle@gmail.com>
2023-12-26 01:35:04 +0000

    Add primeiro scripts R

---
+++ Basic_R.R
@@ -0,0 +1,26 @@
+# Primeiras manipulações com R
+
+4+8
+
+(7+8*2)/2
+
+# Cálculo de Potência
+4**2
+
+# Função raiz quadrada
+sqrt(4)
+
+# Criando objetos
+n <- 100
+n
+n <- 100/3+4
+n
+
+# Exibindo nomes de objetos
+n <- 4
+m <- 2
+s <- 4+2
+nom <- "Dupont"
+ls()
+
+ls.str()
